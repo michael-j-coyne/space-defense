2b83c47a030b3783d7201bd6f90ff42101cee83e
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-01-03 15:13:51 -0500

    Create Weapon

---
+++ weapon.gd
@@ -0,0 +1,45 @@
+class_name Weapon extends Node2D
+
+var _cooldown := 1.0
+var _base_damage := 1.0
+var _damage_multiplier := 1.0
+var _cooling_down = false
+var _projectile_scene = preload("res://projectile.tscn")
+
+func _ready():
+	$Timer.wait_time = _cooldown
+	$Timer.connect("timeout", _cooldown_finished)
+
+func _cooldown_finished():
+	_cooling_down = false
+
+func set_cooldown(cd: float):
+	_cooldown = cd
+
+func fire() -> void:
+	if _cooling_down:
+		return
+
+	var projectile = _projectile_scene.instantiate()
+
+	# TODO: remove harcoded velocity func
+	var velocity_func = func(_delta): return Vector2(0, -5)
+
+	projectile.setup(
+		velocity_func,
+		_base_damage * _damage_multiplier,
+		_cooldown)
+
+	# TEMP, JANKY
+	projectile.global_position = to_global(position)
+
+	# careful here...
+	get_tree().root.add_child(projectile)
+
+	$Timer.start()
+	_cooling_down = true
+
+# TESTING CODE?
+func _input(event):
+	if Input.is_action_pressed("ui_accept"):
+		fire()
