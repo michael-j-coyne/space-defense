f1eddced608461dd0663fe7b32144fccd783a5af
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-01-03 09:07:22 -0500

    Modify Player sprite texture

--- player.gd
+++ player.gd
@@ -1,7 +1,19 @@
 class_name Player extends Area2D
 
+const PLAYER_SIZE = Vector2(100, 100)
 @export var speed = 400
 var velocity = Vector2.ZERO
+
+func _ready():
+	var sprite = get_node("Sprite2D")
+	var collision = get_node("CollisionPolygon2D")
+	var scaling = PLAYER_SIZE / sprite.texture.get_size()
+	
+	# if this is not true, the collision shape scaling will behave improperly
+	assert(scaling.x == scaling.y)
+	
+	collision.scale = scaling
+	sprite.scale = scaling
 
 func _physics_process(delta):
 	velocity = Vector2.ZERO
