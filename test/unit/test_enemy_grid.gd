5c9c758c79fb479378380c2d69a08fd285b21611
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-01-04 13:24:43 -0500

    Fix broken tests by renaming variables
    Tests broke as a result of re-factoring code from main into enemy_grid

--- test/unit/test_enemy_grid.gd
+++ test/unit/test_enemy_grid.gd
@@ -4,24 +4,24 @@ class TestEnemyPosition:
 	extends GutTest
 
 	func test_start():
-		var result = Main.enemy_position(0, 0)
-		assert_eq(Vector2(Main.ENEMY_SIZE.x, Main.ENEMY_SIZE.y) * Vector2(0.5, 0.5), result)
+		var result = EnemyGrid._enemy_position(0, 0)
+		assert_eq(Vector2(EnemyGrid.ENEMY_SIZE.x, EnemyGrid.ENEMY_SIZE.y) * Vector2(0.5, 0.5), result)
 
 	func test_end():
-		var result = Main.enemy_position(Main.NUM_ENEMIES.y - 1, Main.NUM_ENEMIES.x - 1)
+		var result = EnemyGrid._enemy_position(EnemyGrid.NUM_ENEMIES.y - 1, EnemyGrid.NUM_ENEMIES.x - 1)
 		assert_eq(
 			Vector2(
-				Main.ENEMY_GROUP_SIZE.x - (0.5 * Main.ENEMY_SIZE.x),
-				Main.ENEMY_GROUP_SIZE.y - (0.5 * Main.ENEMY_SIZE.y)),
+				EnemyGrid.ENEMY_GROUP_SIZE.x - (0.5 * EnemyGrid.ENEMY_SIZE.x),
+				EnemyGrid.ENEMY_GROUP_SIZE.y - (0.5 * EnemyGrid.ENEMY_SIZE.y)),
 			result)
 
 	func test_middle():
-		if Main.NUM_ENEMIES.x < 1 or Main.NUM_ENEMIES.y < 1:
+		if EnemyGrid.NUM_ENEMIES.x < 1 or EnemyGrid.NUM_ENEMIES.y < 1:
 			return
 
-		var result = Main.enemy_position(1, 1)
+		var result = EnemyGrid._enemy_position(1, 1)
 		assert_eq(
 			Vector2(
-				Main.ENEMY_SIZE.x + (0.5 * Main.ENEMY_SIZE.x) + Main.GAP_SIZE.x,
-				Main.ENEMY_SIZE.y + (0.5 * Main.ENEMY_SIZE.y) + Main.GAP_SIZE.y),
+				EnemyGrid.ENEMY_SIZE.x + (0.5 * EnemyGrid.ENEMY_SIZE.x) + EnemyGrid.GAP_SIZE.x,
+				EnemyGrid.ENEMY_SIZE.y + (0.5 * EnemyGrid.ENEMY_SIZE.y) + EnemyGrid.GAP_SIZE.y),
 			result)
