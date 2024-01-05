bed6ae5c06777550da26a2f15269de56c98047a9
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-01-05 13:47:07 -0500

    Use new Player States in Main
    Testing purposes

--- scenes/main.gd
+++ scenes/main.gd
@@ -8,3 +8,4 @@ func _ready() -> void:
 func _physics_process(_delta: float) -> void:
 	if Input.is_action_just_released("ui_text_backspace"):
 		$TransitionCamera.pan_to_camera($InitialCamera, get_node("Player/Camera2D"), 2.0)
+		$Player.change_state("free")
