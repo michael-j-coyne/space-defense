53fe8bf50cf0c6df6fedf2fdcc1e3ef749717f94
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-01-05 11:33:36 -0500

    Use TransitionCamera in Main

--- scenes/main.gd
+++ scenes/main.gd
@@ -4,3 +4,7 @@ class_name Main extends Node
 
 func _ready() -> void:
 	enemy_spawn.spawn_enemies()
+
+func _physics_process(_delta: float) -> void:
+	if Input.is_action_just_released("ui_text_backspace"):
+		$TransitionCamera.pan_to_camera($InitialCamera, get_node("Player/Camera2D"), 2.0)
