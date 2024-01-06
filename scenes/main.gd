cfe692e6662ffdc5f95c429ec46eb04d3c1fa247
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-01-06 10:20:20 -0500

    When an enemy is killed, increment the sccore
    Main handles the signal emitted from EnemyGrid

--- scenes/main.gd
+++ scenes/main.gd
@@ -3,6 +3,7 @@ const enemy_grid_scene = preload("res://scenes/enemy_grid.tscn")
 const TIME_BETWEEN_ROUNDS = 3.0
 
 var enemy_grid: EnemyGrid
+var score := 0
 
 func _ready() -> void:
 	create_enemy_grid()
@@ -10,12 +11,16 @@ func _ready() -> void:
 func create_enemy_grid():
 	enemy_grid = enemy_grid_scene.instantiate()
 	enemy_grid.all_enemies_defeated.connect(_on_enemy_grid_all_enemies_defeated)
+	enemy_grid.enemy_killed.connect(_on_enemy_killed)
 	add_child(enemy_grid)
 
 func _physics_process(_delta: float) -> void:
 	if Input.is_action_just_released("ui_text_backspace"):
 		$TransitionCamera.pan_to_camera($InitialCamera, get_node("Player/Camera2D"), 2.0)
 		$Player.change_state("free")
+
+func _on_enemy_killed(enemy_info: Shooter.EnemyInfo) -> void:
+	score += enemy_info.value
 
 func _on_enemy_grid_all_enemies_defeated() -> void:
 	enemy_grid.queue_free()
