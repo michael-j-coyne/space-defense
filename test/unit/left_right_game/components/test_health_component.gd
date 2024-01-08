f38383d42601cddf9a37de6dd102bde2a42e86d0
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-01-08 16:56:15 -0500

    Modify test to use class instead of scene

--- test/unit/left_right_game/components/test_health_component.gd
+++ test/unit/left_right_game/components/test_health_component.gd
@@ -1,13 +1,14 @@
 extends GutTest
 
-const health_component = preload("res://left_right_game/components/health_component.tscn")
 
 func test_damage():
-	var hc = health_component.instantiate()
+	var hc = HealthComponent.new()
 
 	var attack = Attack.new()
-	attack.damage_value = hc.health - 0.5
+	attack.damage_value = hc.MAX_HEALTH - 0.5
 
 	hc.damage(attack)
 
-	assert_eq(hc.health, hc.health - 0.5)
+	assert_eq(hc.health, hc.MAX_HEALTH - 0.5)
+
+	hc.free()
