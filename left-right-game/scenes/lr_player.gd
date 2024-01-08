d9d02c14b6137c626248d60f9d433c240cb99051
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-01-08 15:37:36 -0500

    Add ability to move Player

--- left-right-game/scenes/lr_player.gd
+++ left-right-game/scenes/lr_player.gd
@@ -1,1 +1,16 @@
 class_name LRPlayer extends Node2D
+
+@export var speed: float
+
+func _physics_process(delta: float) -> void:
+	var velocity = Vector2.ZERO
+
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+
+	position += velocity * delta
