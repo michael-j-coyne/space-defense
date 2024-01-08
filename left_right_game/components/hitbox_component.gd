f3c5e0cd9dce076a5fbb7cd37ad0414229f3e9e7
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-01-08 17:45:09 -0500

    Rename HitboxComponent member variable
    .damage() -> .take_damage()

--- left_right_game/components/hitbox_component.gd
+++ left_right_game/components/hitbox_component.gd
@@ -2,6 +2,6 @@ class_name HitboxComponent extends Area2D
 
 @export var health_component: HealthComponent
 
-func damage(attack: Attack):
+func take_damage(attack: Attack):
 	if health_component:
 		health_component.take_damage(attack)
