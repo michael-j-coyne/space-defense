0c987bd79ea645cfeb8f5dd00564d48f870ef0c5
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-01-09 10:10:31 -0500

    Complete tests for HealthComponent

--- test/unit/components/test_health_component.gd
+++ test/unit/components/test_health_component.gd
@@ -1,7 +1,7 @@
 extends GutTest
 
 
-func test_damage():
+func test_damage_no_kill():
 	var hc = HealthComponent.new()
 
 	var attack = Attack.new()
@@ -11,3 +11,32 @@ func test_damage():
 	assert_eq(hc.health, hc.MAX_HEALTH - 0.5)
 
 	hc.free()
+
+func test_deadly_damage():
+	var parent = Node.new()
+	var hc = HealthComponent.new()
+	parent.add_child(hc)
+	hc.MAX_HEALTH = 1.0
+	hc.health = 1.0
+
+	var attack = Attack.new()
+	attack.damage = hc.health + 0.5
+	hc.take_damage(attack)
+
+	await wait_frames(2)
+	assert_freed(parent)
+
+func test_deadly_damage_boundary():
+	var parent = Node.new()
+	var hc = HealthComponent.new()
+	parent.add_child(hc)
+	hc.MAX_HEALTH = 1.0
+	hc.health = 1.0
+
+	var attack = Attack.new()
+	# boundary is that health should reach 0.0, but not go below
+	attack.damage = hc.health
+	hc.take_damage(attack)
+
+	await wait_frames(2)
+	assert_freed(parent)
