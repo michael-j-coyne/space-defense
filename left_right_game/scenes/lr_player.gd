920b04fc03d54d3604fb0f2fded9e28a5094c31c
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-01-08 19:28:39 -0500

    Shoot when you press space

--- left_right_game/scenes/lr_player.gd
+++ left_right_game/scenes/lr_player.gd
@@ -9,6 +9,9 @@ func _physics_process(delta: float) -> void:
 		velocity.x += 1
 	if Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
+	if Input.is_action_just_released("ui_accept"):
+		# TODO: fix hardcoded Vector2 (which is velocity of projectile)... and figure out how I want to handle this
+		$GunComponent.shoot(Vector2(0, 1))
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
