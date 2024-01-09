5c715095b84c100c6644cda3dbdc18f9b7cba742
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-01-09 12:47:05 -0500

    Projectile frees itself after given time interval

--- standalone-scripts/projectile.gd
+++ standalone-scripts/projectile.gd
@@ -4,6 +4,10 @@ class_name Projectile extends Node2D
 var attack: Attack
 var speed: float
 var direction: Vector2
+@export var time_alive: float = 2.0
+
+func _ready() -> void:
+	get_tree().create_timer(time_alive).connect("timeout", func(): queue_free())
 
 func _physics_process(delta: float) -> void:
 	var velocity = direction * speed
