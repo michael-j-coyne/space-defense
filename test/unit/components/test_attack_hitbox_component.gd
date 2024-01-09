890e50be19a1de7e485af6f6cb3b4937311d57c9
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-01-09 13:38:47 -0500

    Use double to test the HitboxComponent
    I may have liked the old test better because double() causes a lot of debugger errors it seems.

--- test/unit/components/test_attack_hitbox_component.gd
+++ test/unit/components/test_attack_hitbox_component.gd
@@ -4,7 +4,6 @@ extends GutTest
 # that AttachHitboxComponent is coupled to multiple things so its not too surprising
 func test_on_area_entered():
 	var attack = Attack.new()
-	attack.damage = 0.5
 
 	var attack_hitbox = AttackHitboxComponent.new()
 
@@ -12,17 +11,13 @@ func test_on_area_entered():
 	projectile.attack = attack
 	projectile.add_child(attack_hitbox)
 
-	var health = HealthComponent.new()
-	health.MAX_HEALTH = 1.0
-	health.health = 1.0
-
-	var hitbox = HitboxComponent.new()
-	hitbox.health_component = health
+	# TODO: the double seems to be causing a ton of errors:
+	# Maybe try using Godot 4.0 or 4.2.
+	# "await" keyword not needed in this case, because the expression isn't a coroutine nor a signal.
+	var hitbox = double(HitboxComponent).new()
 
 	attack_hitbox._on_area_entered(hitbox)
 
-	assert_eq(health.health, 0.5)
+	assert_called(hitbox, "take_damage")
 
-	hitbox.free()
 	projectile.free()
-	health.free()
