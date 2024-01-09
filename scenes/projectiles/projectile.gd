8fa495e85e3ad074d5b89441f23306eb0f21cce7
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-01-09 15:08:59 -0500

    Ensure Projectile direction is a normalized vector
    Or the vector (0, 0)

--- scenes/projectiles/projectile.gd
+++ scenes/projectiles/projectile.gd
@@ -10,6 +10,17 @@ func _ready() -> void:
 	get_tree().create_timer(time_alive).connect("timeout", func(): queue_free())
 
 func _physics_process(delta: float) -> void:
+	# for development, the direction should always be a normalized vector
+	# Note: it may be better to assert this in the "setter" for direction,
+	# but I haven't decided if I want to use properties yet.
+	assert(
+		direction == Vector2(0, 0) or direction.is_normalized(),
+		"direction must be a normalized vector or the vector (0, 0)")
+
+	# assert doesn't work in production build, so lets just normalize it and hope everything
+	# works out
+	direction = direction.normalized()
+
 	var velocity = direction * speed
 	position += velocity * delta
 
