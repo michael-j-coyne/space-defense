1e41185b1d48f68a164e89d6b2959268f3a95841
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-01-08 20:57:47 -0500

    Replace TODO with note to self

--- left_right_game/scenes/lr_player.gd
+++ left_right_game/scenes/lr_player.gd
@@ -10,8 +10,9 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
 	if Input.is_action_just_released("ui_accept"):
-		# TODO: fix hardcoded Vector2 (which is velocity of projectile)... and figure out how I want to handle this
-		$GunComponent.shoot(Vector2(0, 1))
+		# Note: in the future we may want to be able to shoot in a particular direction
+		var direction = Vector2(0, -1).normalized()
+		$GunComponent.shoot(direction)
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
