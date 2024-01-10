4f89ef97f5f6730ec8fd7c57e33dee6116a60db5
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-01-10 13:21:48 -0500

    Add GunComponent config warnings

--- components/gun_component.gd
+++ components/gun_component.gd
@@ -1,8 +1,20 @@
+@tool
 class_name GunComponent extends Node2D
 
 @export var ammo: PackedScene
 @export var projectile_speed := 0.0
 @export var damage := 0.0
+
+func _get_configuration_warnings() -> PackedStringArray:
+	var warnings = PackedStringArray()
+	if not ammo:
+		warnings.append("You have not given the gun any ammo. The gun will be unable to shoot.")
+	if damage == 0:
+		warnings.append("The damage of the gun is 0. The projectiles will not do any damage.")
+	if projectile_speed == 0:
+		warnings.append("The projectile speed is 0. The projectiles will not move.")
+
+	return warnings
 
 func shoot(direction: Vector2) -> Projectile:
 	var attack = Attack.new()
