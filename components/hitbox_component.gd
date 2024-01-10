d258ba97b987f6f1f03f92d5bf0703e10390efe8
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-01-10 13:21:38 -0500

    Add HitboxComponent config warnings

--- components/hitbox_component.gd
+++ components/hitbox_component.gd
@@ -1,6 +1,13 @@
+@tool
 class_name HitboxComponent extends Area2D
 
 @export var health_component: HealthComponent
+
+func _get_configuration_warnings() -> PackedStringArray:
+	var warnings = PackedStringArray()
+	if not health_component:
+		warnings.append("You have not set a health component.")
+	return warnings
 
 func take_damage(attack: Attack):
 	if health_component:
