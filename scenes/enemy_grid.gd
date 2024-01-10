81f9f2d80e0d36be90467996ef2229731abb57e0
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-01-10 11:41:40 -0500

    Begin work on enemy_grid
    Grid incomplete, have some more thinking to do. But this works for now.

--- scenes/enemy_grid.gd
+++ scenes/enemy_grid.gd
@@ -1,8 +1,20 @@
+@tool
 class_name EnemyGrid extends Node2D
 
 @export var num_cols: int = 1
 @export var num_rows: int = 1
+@export var gap_size: Vector2 = Vector2(0, 0)
 @export var enemy: PackedScene
+
+# Note: this actually isn't an enemy grid, its a grid that can position any element
+# that implements the "get_boundary" method. Can I add a warning to check if
+# children implement this method?
+
+func _get_configuration_warnings() -> PackedStringArray:
+	if get_child_count() == 0:
+		return PackedStringArray(["You should add enemies to the grid"])
+	else:
+		return PackedStringArray([])
 
 # Note: I'm not sure this method should actually be exposed, but encapsulating it within another node
 # like "position_calculator" doesn't seem right. I suppose I could create a "library"
@@ -17,6 +29,7 @@ static func enemy_position(row_idx: int, col_idx: int, enemy_size: Vector2, gap_
 
 	return Vector2(pos_x, pos_y)
 
+# TODO: problem: we could return a value greater than the number of allowed rows
 static func row_idx(enemy_idx: int, num_cols: int) -> int:
 	assert(num_cols > 0, "num_cols must be positive")
 	assert(enemy_idx >= 0, "enemy_idx must be non-negative")
@@ -26,3 +39,18 @@ static func col_idx(enemy_idx: int, num_cols: int) -> int:
 	assert(num_cols > 0, "num_cols must be positive")
 	assert(enemy_idx >= 0, "enemy_idx must be non-negative")
 	return enemy_idx % num_cols
+
+# This works for positoning the enemies initially but after some enemies are killed their
+# index get_child_count() will change. So we can't re-position the enemies using this method.
+func set_enemy_initial_positions():
+	# This code only works with grunt right now, we are just messing around.
+	var enemies = get_children()
+	for idx in get_child_count():
+		var enemy = enemies[idx]
+		var row_idx = EnemyGrid.row_idx(idx, num_cols)
+		var col_idx = EnemyGrid.col_idx(idx, num_cols)
+		var pos = EnemyGrid.enemy_position(row_idx, col_idx, enemy.get_boundary(), gap_size)
+		enemy.position = pos
+
+func _ready():
+	set_enemy_initial_positions()
