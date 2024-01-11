dd1fd0d37612f457e158d61182aaa3a20df3fd2b
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-01-10 22:03:30 -0500

    Add cooldown to gun_component

--- components/gun_component.gd
+++ components/gun_component.gd
@@ -1,12 +1,17 @@
 @tool
 class_name GunComponent extends Node2D
 
+## How long before the gun can fire again
+@export var cooldown: float = 1.0
 ## The projectile scene which will be shot from the gun
 @export var ammo: PackedScene
 ## How fast the projectile fired from the gun moves in pixels / second
 @export var projectile_speed := 0.0
 ## How much damage the projectile fired from the gun will do, before any multipliers
 @export var damage := 0.0
+
+# If the gun is cooling down, it can't fire.
+var cooling_down = false
 
 func _get_configuration_warnings() -> PackedStringArray:
 	var warnings = PackedStringArray()
@@ -16,10 +21,15 @@ func _get_configuration_warnings() -> PackedStringArray:
 		warnings.append("The damage of the gun is 0. The projectiles will not do any damage.")
 	if projectile_speed == 0:
 		warnings.append("The projectile speed is 0. The projectiles will not move.")
+	if cooldown < 0:
+		warnings.append("The cooldown must be >= 0")
 
 	return warnings
 
 func shoot(direction: Vector2) -> Projectile:
+	if cooling_down:
+		return
+
 	var attack = Attack.new()
 	attack.damage = damage
 
@@ -37,6 +47,9 @@ func shoot(direction: Vector2) -> Projectile:
 	projectile.global_position = global_position + translation_direction * translation_amount
 
 	get_node("/root").add_child(projectile)
+
+	cooling_down = true
+	get_tree().create_timer(cooldown).connect("timeout", func(): cooling_down = false)
 
 	return projectile
 
