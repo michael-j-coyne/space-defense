9d7e7f9ee694ac289e9a9115036eefe88c8cfe79
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-01-10 22:05:57 -0500

    Allow holding down space to continually fire

--- scenes/player.gd
+++ scenes/player.gd
@@ -11,7 +11,7 @@ func _physics_process(delta: float) -> void:
 		velocity.x += 1
 	if Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
-	if Input.is_action_just_released("ui_accept"):
+	if Input.is_action_pressed("ui_accept"):
 		# Note: in the future we may want to be able to shoot in a particular direction
 		var direction = Vector2(0, -1).normalized()
 		$GunComponent.shoot(direction)
