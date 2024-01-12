d9327d34f6b34476d667d4616b6b40e3c0984377
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-01-12 11:14:26 -0500

    Go to next level when level completed

--- scenes/main.gd
+++ scenes/main.gd
@@ -6,9 +6,24 @@ var levels := [
 ]
 
 var current_level_index = 0
+var current_level: Level
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var level: Level = levels[current_level_index].instantiate() as Level
-	level.completed.connect(func(): print('yay complete!'))
+	level.completed.connect(go_next_level)
+	current_level = level
 	add_child(level)
+
+func go_next_level():
+	if current_level_index == levels.size() - 1:
+		# no more levels
+		return
+
+	current_level.queue_free()
+	current_level_index += 1
+	var level: Level = levels[current_level_index].instantiate() as Level
+	level.completed.connect(go_next_level)
+	current_level = level
+	add_child(level)
+
