a654595ec42a6d6ad171a2dfb92fd8193c2853b3
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-01-12 12:04:38 -0500

    Draw LREMover + use percentage for width
    Draw the LeftRightEntityMover in the editor to make level design easier
    
    Use percentage for the width of the LeftRightEntityMover bounding box rather than pixels, so that if we change the screen width everything still works.

--- components/left_right_entity_mover.gd
+++ components/left_right_entity_mover.gd
@@ -7,12 +7,9 @@ enum anchor_point { upper_left, center }
 ## The entity that will be moved by the LeftRightEntityMover
 @export var entity: Node2D
 
-## The width of the bounding box for the LeftRightEntityMover. When an entity hits the edge
-## of the bounding box, its position is reversed.
-@export var boundary_value: float = 0
-
-# TODO: implement logic for this
-@export var entity_starting_pos: float = 0
+## The width of the bounding box for the LeftRightEntityMover, as a percentage of the total
+## screen width. When an entity hits the edge of the bounding box, its position is reversed.
+@export var box_width_percent: float = 0
 
 ## How far downward the entity will move when it hits the boundary
 @export var vertical_step: float = 0
@@ -30,6 +27,7 @@ var direction := 1 if initial_direction == move_direction.left else -1
 
 func _get_configuration_warnings() -> PackedStringArray:
 	var message = PackedStringArray()
+	var screen_width = ProjectSettings.get_setting("display/window/size/viewport_width")
 
 	if not entity:
 		message.append("You have not specified an entity to move.")
@@ -38,8 +36,11 @@ func _get_configuration_warnings() -> PackedStringArray:
 		message.append("Entity must have a boundary() method that returns a Vector2 representing
 		the node's bounding box")
 
-	if entity and boundary_value <= entity.boundary().x:
+	if entity and screen_width * box_width_percent <= entity.boundary().x:
 		message.append("The boundary value must be greater than the width of the entity.")
+
+	if box_width_percent < 0.0 or box_width_percent > 1.0:
+		message.append("The box width percentage must be between 0 and 1")
 
 	if vertical_step < 0:
 		message.append("The vertical step must be greater than or equal to 0.")
@@ -48,6 +49,25 @@ func _get_configuration_warnings() -> PackedStringArray:
 		message.append("The speed must be greater than or equal to 0.")
 
 	return message
+
+func _draw():
+	# TODO: Only draw the object when it is selected
+	if not Engine.is_editor_hint():
+		return
+
+	var screen_height = ProjectSettings.get_setting("display/window/size/viewport_height")
+	var screen_width = ProjectSettings.get_setting("display/window/size/viewport_width")
+
+	var box_start = Vector2(0, 0)
+	var box_end = Vector2(box_width_percent * screen_width, screen_height)
+
+	draw_rect(Rect2(box_start, box_end), Color(Color.DARK_OLIVE_GREEN, 0.4))
+
+func _process(_delta: float) -> void:
+	if not Engine.is_editor_hint():
+		return
+
+	queue_redraw()
 
 func _physics_process(delta: float) -> void:
 	if Engine.is_editor_hint():
@@ -58,16 +78,17 @@ func _physics_process(delta: float) -> void:
 		queue_free()
 		return
 
+	# NOTE: Do I want to do this on every loop?
+	var screen_width = get_node('/root').content_scale_size.x
+
 	entity.position.x += direction * speed * delta
 
 	if anchor == anchor_point.upper_left:
 
-		# TODO: Extract into a function and test it.
-		# Also, boundary() ends up being called with no children once all the enemies in the grid are dead.
 		if entity.position.x < 0:
 			direction *= -1
 			entity.position.y += vertical_step
-		elif entity.position.x + entity.boundary().x > boundary_value:
+		elif entity.position.x + entity.boundary().x > box_width_percent * screen_width:
 			direction *= -1
 			entity.position.y += vertical_step
 	else:
