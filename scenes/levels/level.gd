6bec969edd2693976ee415f3cc149364fc5d0fd8
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-01-12 11:05:41 -0500

    Add warning if signal not connected to level

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,6 +1,16 @@
+@tool
 class_name Level extends Node2D
 
 signal completed
+
+func _get_configuration_warnings() -> PackedStringArray:
+	var warnings = PackedStringArray()
+	# NOTE: what if there are multiple signals that need to be connected?
+	# how will we check for each one?
+	if not get_incoming_connections():
+		warnings.append("No signals detected. Make sure to connect a signal from the Enemies so
+		that we can determine when all of the enemies have been killed.")
+	return warnings
 
 func _on_all_enemies_defeated():
 	completed.emit()
