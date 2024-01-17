b8378dff8bf56a3a988bcad913e94516fb82c3ff
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-01-17 13:38:56 -0500

    Add reset function to autoload singletons

--- player_variables.gd
+++ player_variables.gd
@@ -1,9 +1,21 @@
 extends Node
 
-var money: int = 0
-var speed: float = 600
+var money: int
+var speed: float
 
 # gun variables
-var gun_cooldown = 0.75
-var gun_projectile_speed = 750
-var gun_penetrations := 1
+var gun_cooldown: float
+var gun_projectile_speed: float
+var gun_penetrations: int
+
+func _ready() -> void:
+	reset()
+
+func reset():
+	money = 0
+	speed = 600
+
+	# gun variables
+	gun_cooldown = 0.75
+	gun_projectile_speed = 750
+	gun_penetrations = 1
