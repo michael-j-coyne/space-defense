2c7b8e009685932f6ccd458dcbfba7c0ac5e9bb8
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-01-24 12:02:45 -0500

    Send retry signal in game over screen
    Signal sends on enter pressed

--- screens/game_over.gd
+++ screens/game_over.gd
@@ -1,11 +1,12 @@
 extends Sprite2D
 
 signal new_game_requested
+signal retry_requested
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
-		new_game_requested.emit()
+		retry_requested.emit()
 
 func _on_button_pressed() -> void:
 	new_game_requested.emit()
