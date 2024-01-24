8dd07f2b2d7756d08fbf1bfa53f62be3c2dd04d4
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-01-24 12:39:58 -0500

    Make "retry" text fade in/out

--- screens/game_over.gd
+++ screens/game_over.gd
@@ -3,6 +3,11 @@ extends Sprite2D
 signal new_game_requested
 signal retry_requested
 
+var tween_time = 1.0
+
+func _ready():
+	button_flash()
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
@@ -10,3 +15,20 @@ func _process(_delta: float) -> void:
 
 func _on_button_pressed() -> void:
 	new_game_requested.emit()
+
+func button_flash():
+	button_fadeout()
+
+func button_fadeout():
+	var tween = $RichTextLabel2.create_tween()
+	tween.set_trans(Tween.TRANS_CUBIC)
+	tween.set_ease(Tween.EASE_IN)
+	tween.tween_property($RichTextLabel2, "modulate:a", 0, tween_time)
+	tween.tween_callback(button_fadein)
+
+func button_fadein():
+	var tween = $RichTextLabel2.create_tween()
+	tween.set_trans(Tween.TRANS_CUBIC)
+	tween.set_ease(Tween.EASE_OUT)
+	tween.tween_property($RichTextLabel2, "modulate:a", 1, tween_time)
+	tween.tween_callback(button_fadeout)
