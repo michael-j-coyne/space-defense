fa8bb49d59a42550bd6d820943b13d3f02b6db02
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-01-24 20:34:38 -0500

    Enemy grid handles its own movement

--- scenes/enemy_grid.gd
+++ scenes/enemy_grid.gd
@@ -1,10 +1,31 @@
 @tool
 class_name EnemyGrid extends Node2D
 
+enum move_direction { left, right }
+
 @export var num_cols: int = 1
 @export var num_rows: int = 1
 @export var gap_size: Vector2 = Vector2(0, 0)
 @export var clone_first_child: bool = false
+
+## The width of the bounding box for the LeftRightEntityMover, as a percentage of the total
+## screen width. When an entity hits the edge of the bounding box, its position is reversed.
+@export var box_width_percent: float = 0
+
+@export var enemy_start_pos: float = 0.0
+
+## How far downward the entity will move when it hits the boundary
+@export var vertical_step: float = 0
+
+## The speed at which the entity will move, in pixels / second
+@export var speed: float = 0
+
+## The intial x direction that the entity moves
+@export var initial_direction: move_direction = move_direction.left
+
+var direction := 1 if initial_direction == move_direction.left else -1
+
+var enemy_grid_pos: float
 
 # COUPLING: requires that children implement boundary()
 
@@ -34,6 +55,7 @@ func _get_configuration_warnings() -> PackedStringArray:
 
 func _ready() -> void:
 	set_enemy_initial_positions()
+	enemy_grid_pos = enemy_start_pos
 
 # NOTE: I'm not sure this method should actually be exposed, but encapsulating it within another node
 # like "position_calculator" doesn't seem right. I suppose I could create a "library"
@@ -62,6 +84,7 @@ static func col_idx(enemy_idx: int, num_cols: int) -> int:
 # This works for positoning the enemies initially but after some enemies are killed their
 # index will change. So we can't re-position the enemies using this method.
 func set_enemy_initial_positions():
+	var screen_width = ProjectSettings.get_setting("display/window/size/viewport_width")
 	var enemies = get_children()
 	for idx in get_child_count():
 		var enemy = enemies[idx]
@@ -69,6 +92,7 @@ func set_enemy_initial_positions():
 		var col_idx = EnemyGrid.col_idx(idx, num_cols)
 		var pos = EnemyGrid.enemy_position(row_idx, col_idx, enemy.boundary(), gap_size)
 		enemy.position = pos
+		enemy.position.x += enemy_start_pos
 
 static func calculate_grid_size(total_num_rows: int, total_num_cols: int, enemy_size: Vector2, gap_size: Vector2) -> Vector2:
 	assert(total_num_rows > 0, "num_rows must be positive")
@@ -95,18 +119,36 @@ func create_afterimage() -> void:
 	for enemy in get_children():
 		enemy.create_afterimage()
 
+func fill_grid():
+	var enemy = get_child(0)
+	var new_enemy = enemy.duplicate(DUPLICATE_SCRIPTS)
+	add_child(new_enemy)
+	new_enemy.set_owner(enemy.get_owner())
+
+func _draw():
+	# TODO: Only draw the object when it is selected
+	if not Engine.is_editor_hint():
+		return
+
+	var screen_height = ProjectSettings.get_setting("display/window/size/viewport_height")
+	var screen_width = ProjectSettings.get_setting("display/window/size/viewport_width")
+
+	var box_start = Vector2(0, 0)
+	var box_end = Vector2(box_width_percent * screen_width, screen_height)
+
+	draw_rect(Rect2(box_start, box_end), Color(Color.DARK_OLIVE_GREEN, 0.4))
+
 func _process(_delta):
 	if Engine.is_editor_hint():
 		if clone_first_child and get_child_count() > 0 and get_child_count() < num_rows * num_cols:
-			var enemy = get_child(0)
-			var new_enemy = enemy.duplicate(DUPLICATE_SCRIPTS)
-			add_child(new_enemy)
-			new_enemy.set_owner(enemy.get_owner())
+			fill_grid()
 
+		# remove children from grid
 		if get_child_count() > num_rows * num_cols and get_child_count() > 1:
 			remove_child(get_children()[-1])
 
 		set_enemy_initial_positions()
+		queue_redraw()
 		return
 
 	if Input.is_action_just_pressed("ui_text_backspace"):
@@ -118,3 +160,28 @@ func _process(_delta):
 	# boundary()
 	if get_child_count() < 1:
 		queue_free()
+#
+func move_enemies(amount: float):
+	for enemy in get_children():
+		enemy.position.x += amount
+
+func _physics_process(delta: float) -> void:
+	if Engine.is_editor_hint():
+		return
+
+	# NOTE: Do I want to do this on every loop?
+	var screen_width = get_node("/root").content_scale_size.x
+
+	var move_amount = direction * speed * delta
+	move_enemies(move_amount)
+	enemy_grid_pos += move_amount
+
+
+	if enemy_grid_pos < 0:
+		direction *= -1
+		create_afterimage()
+		position.y += vertical_step
+	elif enemy_grid_pos + boundary().x > box_width_percent * screen_width:
+		direction *= -1
+		create_afterimage()
+		position.y += vertical_step
