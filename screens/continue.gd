2455517efaa13435965f66aa1f47c7d5fa95202c
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-01-26 16:02:09 -0500

    Add confirmation dialog for starting a new game

--- screens/continue.gd
+++ screens/continue.gd
@@ -11,11 +11,10 @@ func _ready():
 func _process(_delta):
 	if Input.is_action_just_pressed("ui_accept"):
 		continue_game.emit()
-	if Input.is_action_just_pressed("ui_text_backspace"):
-		new_game.emit()
 
 func _on_new_game_pressed() -> void:
-	new_game.emit()
+	$ConfirmationDialog.visible = true
+	$ConfirmationDialog.confirmed.connect(func(): new_game.emit())
 
 func _on_continue_pressed() -> void:
 	continue_game.emit()
