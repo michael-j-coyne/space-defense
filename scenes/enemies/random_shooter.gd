512554a80977d4a55b2769158d46da3593e5dc2e
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-01-26 15:31:57 -0500

    Add option for enemy to start shooting after delay

--- scenes/enemies/random_shooter.gd
+++ scenes/enemies/random_shooter.gd
@@ -6,8 +6,8 @@ class_name RandomShooter extends Enemy
 
 @export var shot_chance_percentage: float
 @export var shot_interval_seconds: float
+@export var start_shooting_delay := 0.0
 var rng = RandomNumberGenerator.new()
-
 
 func _ready():
 	if Engine.is_editor_hint():
@@ -23,4 +23,4 @@ func _ready():
 				$GunComponent.shoot(Vector2(0, 1))
 	)
 	add_child(timer)
-	timer.start()
+	get_tree().create_timer(start_shooting_delay).timeout.connect(timer.start)
