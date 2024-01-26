6a35f9db456ad94851d61180a542882421338f2f
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-01-26 15:44:30 -0500

    Refactor shop items + remove gatling gun for now

--- components/shop_item.gd
+++ components/shop_item.gd
@@ -7,12 +7,3 @@ var	stock: int = 0
 var	max_stock: int = 0
 var	price_multiplier: int = 0
 var	apply_upgrade: Callable
-
-func _init(display_text, description, base_price, stock, max_stock, price_multiplier, apply_upgrade):
-	self.display_text = display_text
-	self.description = description
-	self.base_price = base_price
-	self.stock = stock
-	self.max_stock = max_stock
-	self.price_multiplier = price_multiplier
-	self.apply_upgrade = apply_upgrade
--- globals.gd
+++ globals.gd
@@ -8,53 +8,49 @@ var shop_inventory
 func _ready():
 	reset()
 
-var attack_speed_upgrade = ShopItem.new(
-	"Attack speed +30%",
-	"Shoot faster. Who can hate that?",
-	600,
-	2,
-	2,
-	5,
-	func(): PlayerVariables.gun_cooldown *= .7)
-
-var movement_speed_upgrade = ShopItem.new(
-	"Movement Speed +50%",
-	"\"Gotta go fast!\"",
-	600,
-	2,
-	2,
-	5,
-	func(): PlayerVariables.speed *= 1.5)
-
-var piercing_shot_upgrade = ShopItem.new(
-	"Piercing shots +1",
-	"Destroy two ships with one laser!",
-	600,
-	3,
-	3,
-	5,
-	func(): PlayerVariables.gun_penetrations += 1)
-
-var gatling_gun_upgrade = ShopItem.new(
-	"Gatling Gun",
-	"Now that's just plain fun.",
-	2000,
-	1,
-	1,
-	10,
-	func(): PlayerVariables.has_gatling_gun = true)
-
 func reset():
-	attack_speed_upgrade.stock = 2
-	movement_speed_upgrade.stock = 2
-	piercing_shot_upgrade.stock = 3
-	gatling_gun_upgrade.stock = 1
+	var attack_speed_upgrade = ShopItem.new()
+	attack_speed_upgrade.display_text = "Attack speed +30%"
+	attack_speed_upgrade.description = "Shoot faster. Who can hate that?"
+	attack_speed_upgrade.base_price = 600
+	attack_speed_upgrade.stock = 1
+	attack_speed_upgrade.max_stock = 1
+	attack_speed_upgrade.price_multiplier = 5
+	# upgrade.effect is a better name than apply_upgrade
+	attack_speed_upgrade.apply_upgrade = func(): PlayerVariables.gun_cooldown *= .7
+
+	var movement_speed_upgrade = ShopItem.new()
+	movement_speed_upgrade.display_text = "Movement speed +50%"
+	movement_speed_upgrade.description = "Gotta go fast!"
+	movement_speed_upgrade.base_price = 600
+	movement_speed_upgrade.stock = 1
+	movement_speed_upgrade.max_stock = 1
+	movement_speed_upgrade.price_multiplier = 5
+	movement_speed_upgrade.apply_upgrade = func(): PlayerVariables.speed *= 1.5
+
+	var piercing_shot_upgrade = ShopItem.new()
+	piercing_shot_upgrade.display_text = "Piercing shots +1"
+	piercing_shot_upgrade.description = "Destroy two ships with one laser!"
+	piercing_shot_upgrade.base_price = 600
+	piercing_shot_upgrade.stock = 1
+	piercing_shot_upgrade.max_stock = 1
+	piercing_shot_upgrade.price_multiplier = 5
+	piercing_shot_upgrade.apply_upgrade = func(): PlayerVariables.gun_penetrations += 1
+
+	# var gatling_gun_upgrade = ShopItem.new()
+	# gatling_gun_upgrade.display_text = "Gatling Gun"
+	# gatling_gun_upgrade.description = "Now that's just plain fun."
+	# gatling_gun_upgrade.base_price = 2000
+	# gatling_gun_upgrade.stock = 1
+	# gatling_gun_upgrade.max_stock = 1
+	# gatling_gun_upgrade.price_multiplier = 10
+	# gatling_gun_upgrade.apply_upgrade = func(): PlayerVariables.has_gatling_gun = true
 
 	shop_inventory = {
 		"attack_speed_upgrade": attack_speed_upgrade,
 		"movement_speed_upgrade": movement_speed_upgrade,
 		"piercing_shot_upgrade": piercing_shot_upgrade,
-		"gatling_gun_upgrade": gatling_gun_upgrade,
+		# "gatling_gun_upgrade": gatling_gun_upgrade,
 	}
 
 	current_level = null
@@ -66,10 +62,10 @@ func save():
 		"current_level_idx": current_level_idx,
 		"in_shop": in_shop,
 		"shop_stock": {
-			"attack_speed_upgrade": attack_speed_upgrade.stock,
-			"movement_speed_upgrade": movement_speed_upgrade.stock,
-			"piercing_shot_upgrade": piercing_shot_upgrade.stock,
-			"gatling_gun_upgrade": gatling_gun_upgrade.stock,
+			"attack_speed_upgrade": shop_inventory["attack_speed_upgrade"].stock,
+			"movement_speed_upgrade": shop_inventory["movement_speed_upgrade"].stock,
+			"piercing_shot_upgrade": shop_inventory["piercing_shot_upgrade"].stock,
+			# "gatling_gun_upgrade": gatling_gun_upgrade.stock,
 		}
 	}
 
