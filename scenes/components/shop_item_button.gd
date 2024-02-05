ea27fb6cf06dfee8ab108b305c0ff0fe1fd32759
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-02-05 17:36:02 -0500

    Use theme colors in ShopItemButton

--- scenes/components/shop_item_button.gd
+++ scenes/components/shop_item_button.gd
@@ -4,10 +4,17 @@ extends VBoxContainer
 @export var item_label: Label
 @export var description_label: Label
 
+var hover_color
+var focus_color
+
 func _ready():
 	if $ShopItemButton.disabled:
 		item_label.modulate = (Color(0.5, 0.5, 0.5))
 		price_label.modulate = (Color(0.5, 0.5, 0.5))
+
+	var theme_resource := load(ProjectSettings.get("gui/theme/custom")) as Theme
+	hover_color = theme_resource.get_color("font_hover_color", "Button")
+	focus_color = theme_resource.get_color("font_focus_color", "Button")
 
 func get_button() -> Button:
 	return $ShopItemButton
@@ -20,3 +27,20 @@ func set_price(price: int):
 
 func set_description(description: String):
 	description_label.text = "  " + description
+
+
+func _on_shop_item_button_focus_entered() -> void:
+	price_label.add_theme_color_override("font_color", focus_color)
+	item_label.add_theme_color_override("font_color", focus_color)
+
+func _on_shop_item_button_focus_exited() -> void:
+	price_label.remove_theme_color_override("font_color")
+	item_label.remove_theme_color_override("font_color")
+
+func _on_shop_item_button_mouse_entered() -> void:
+	price_label.add_theme_color_override("font_color", hover_color)
+	item_label.add_theme_color_override("font_color", hover_color)
+
+func _on_shop_item_button_mouse_exited() -> void:
+	price_label.remove_theme_color_override("font_color")
+	item_label.remove_theme_color_override("font_color")
