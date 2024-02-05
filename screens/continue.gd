d5b9b4ac2845cf629ca2c61b5cd4f6ee79575825
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-02-05 14:40:16 -0500

    Refactor continue screen
    Allow for switching focus between "new game" and "continue" buttons with tab

--- screens/continue.gd
+++ screens/continue.gd
@@ -1,4 +1,4 @@
-extends Sprite2D
+extends TextureRect
 
 signal new_game
 signal continue_game
@@ -6,11 +6,12 @@ signal continue_game
 var tween_time = 1.0
 
 func _ready():
-	NodeFlasher.node_flash($ContinueButton, tween_time)
+	$ContinueButton.grab_focus()
 
-func _process(_delta):
-	if Input.is_action_just_pressed("ui_accept"):
-		continue_game.emit()
+#func _process(_delta):
+	#if Input.is_action_just_pressed("ui_accept"):
+		#if not $ConfirmationDialog.visible:
+			#continue_game.emit()
 
 func _on_new_game_pressed() -> void:
 	$ConfirmationDialog.visible = true
@@ -18,3 +19,12 @@ func _on_new_game_pressed() -> void:
 
 func _on_continue_pressed() -> void:
 	continue_game.emit()
+
+
+func _on_new_game_button_mouse_entered() -> void:
+	$NewGameButton.grab_focus()
+
+
+
+func _on_continue_button_mouse_entered() -> void:
+	$ContinueButton.grab_focus()
