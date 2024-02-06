7927575bd8882f859d3aac4eac56eabe9b18681b
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-02-05 19:37:36 -0500

    Format continue.gd, remove unused code

--- screens/continue.gd
+++ screens/continue.gd
@@ -8,11 +8,6 @@ var tween_time = 1.0
 func _ready():
 	$ContinueButton.grab_focus()
 
-#func _process(_delta):
-	#if Input.is_action_just_pressed("ui_accept"):
-		#if not $ConfirmationDialog.visible:
-			#continue_game.emit()
-
 func _on_new_game_pressed() -> void:
 	$ConfirmationDialog.visible = true
 	$ConfirmationDialog.confirmed.connect(func(): new_game.emit())
@@ -20,11 +15,8 @@ func _on_new_game_pressed() -> void:
 func _on_continue_pressed() -> void:
 	continue_game.emit()
 
-
 func _on_new_game_button_mouse_entered() -> void:
 	$NewGameButton.grab_focus()
-
-
 
 func _on_continue_button_mouse_entered() -> void:
 	$ContinueButton.grab_focus()
