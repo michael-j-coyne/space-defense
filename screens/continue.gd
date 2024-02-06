7a2f21adcdf8a9619fc14a6fe5813b168df93801
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-02-06 18:26:08 -0500

    Use export vars in continue.gd
    That way when we restructure the tree, we don't have to go into the script and edit node paths.

--- screens/continue.gd
+++ screens/continue.gd
@@ -4,19 +4,22 @@ signal new_game
 signal continue_game
 
 var tween_time = 1.0
+@export var continue_button: Button
+@export var new_game_button: Button
+@export var confirmation_dialog: ConfirmationDialog
 
 func _ready():
-	$ContinueButton.grab_focus()
+	continue_button.grab_focus()
 
 func _on_new_game_pressed() -> void:
-	$ConfirmationDialog.visible = true
-	$ConfirmationDialog.confirmed.connect(func(): new_game.emit())
+	confirmation_dialog.visible = true
+	confirmation_dialog.confirmed.connect(func(): new_game.emit())
 
 func _on_continue_pressed() -> void:
 	continue_game.emit()
 
 func _on_new_game_button_mouse_entered() -> void:
-	$NewGameButton.grab_focus()
+	new_game_button.grab_focus()
 
 func _on_continue_button_mouse_entered() -> void:
-	$ContinueButton.grab_focus()
+	continue_button.grab_focus()
