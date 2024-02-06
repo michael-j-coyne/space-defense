531f99fa6cec72bf3c8adf0bc58b534471983ea6
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-02-06 16:12:56 -0500

    Refactor + redesign GameOver screen
    Cleaner UI + tab focusing
    
    Remove node flashing

--- screens/game_over.gd
+++ screens/game_over.gd
@@ -5,35 +5,16 @@ signal retry_requested
 signal shop_requested
 
 var tween_time = 1.0
+@onready var retry_button = get_node("VBoxContainer/RetryButton")
 
 func _ready():
-	button_flash()
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
-		retry_requested.emit()
+	retry_button.grab_focus()
 
 func _on_button_pressed() -> void:
 	new_game_requested.emit()
 
-func button_flash():
-	button_fadeout()
-
-func button_fadeout():
-	var tween = $RichTextLabel2.create_tween()
-	tween.set_trans(Tween.TRANS_CUBIC)
-	tween.set_ease(Tween.EASE_IN)
-	tween.tween_property($RichTextLabel2, "modulate:a", 0, tween_time)
-	tween.tween_callback(button_fadein)
-
-func button_fadein():
-	var tween = $RichTextLabel2.create_tween()
-	tween.set_trans(Tween.TRANS_CUBIC)
-	tween.set_ease(Tween.EASE_OUT)
-	tween.tween_property($RichTextLabel2, "modulate:a", 1, tween_time)
-	tween.tween_callback(button_fadeout)
-
-
 func _on_shop_button_pressed() -> void:
 	shop_requested.emit()
+
+func _on_retry_button_pressed() -> void:
+	retry_requested.emit()
