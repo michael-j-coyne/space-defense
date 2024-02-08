f9a875a5508fea40f4602c057f7768f26bd692a3
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-02-08 17:33:13 -0500

    Make items cheaper

--- singletons/globals.gd
+++ singletons/globals.gd
@@ -11,23 +11,23 @@ func _ready():
 
 func reset():
 	var attack_speed_upgrade = ShopItem.new()
-	attack_speed_upgrade.display_text = "Attack speed +30%"
+	attack_speed_upgrade.display_text = "Attack speed +10%"
 	attack_speed_upgrade.description = "Shoot faster. Who can hate that?"
-	attack_speed_upgrade.base_price = 1000
-	attack_speed_upgrade.stock = 1
-	attack_speed_upgrade.max_stock = 1
-	attack_speed_upgrade.price_multiplier = 5
+	attack_speed_upgrade.base_price = 100
+	attack_speed_upgrade.stock = 3
+	attack_speed_upgrade.max_stock = 3
+	attack_speed_upgrade.price_multiplier = 1.5
 	# upgrade.effect is a better name than apply_upgrade
-	attack_speed_upgrade.apply_upgrade = func(): PlayerVariables.gun_cooldown *= .7
+	attack_speed_upgrade.apply_upgrade = func(): PlayerVariables.gun_cooldown *= .9
 
 	var movement_speed_upgrade = ShopItem.new()
-	movement_speed_upgrade.display_text = "Movement speed +50%"
+	movement_speed_upgrade.display_text = "Movement speed +20%"
 	movement_speed_upgrade.description = "Gotta go fast!"
-	movement_speed_upgrade.base_price = 400
-	movement_speed_upgrade.stock = 1
-	movement_speed_upgrade.max_stock = 1
-	movement_speed_upgrade.price_multiplier = 5
-	movement_speed_upgrade.apply_upgrade = func(): PlayerVariables.speed *= 1.5
+	movement_speed_upgrade.base_price = 100
+	movement_speed_upgrade.stock = 2
+	movement_speed_upgrade.max_stock = 2
+	movement_speed_upgrade.price_multiplier = 1.5
+	movement_speed_upgrade.apply_upgrade = func(): PlayerVariables.speed *= 1.20
 
 	var piercing_shot_upgrade = ShopItem.new()
 	piercing_shot_upgrade.display_text = "Piercing shots +1"
