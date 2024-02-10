c0aa718bd4900551679c08bee1fff31d51d7b9bb
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-02-10 14:34:25 -0500

    temp...... undo this

--- scenes/components/stat_display.gd
+++ scenes/components/stat_display.gd
@@ -1,7 +1,9 @@
-extends PanelContainer
+extends Node2D
 
 @export var attack_speed_val: Label
 @export var move_speed_val: Label
+
+
 
 var gun_cooldown: float
 
@@ -14,16 +16,31 @@ func _ready() -> void:
 		attack_speed_val.text = "%.2f shots / sec" % (1.0 / PlayerVariables.gun_cooldown)
 		gun_cooldown = PlayerVariables.gun_cooldown
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if not PlayerVariables:
-		return
-	if PlayerVariables.gun_cooldown and gun_cooldown:
-
-		if PlayerVariables.gun_cooldown != gun_cooldown:
-			var difference = (1.0 / PlayerVariables.gun_cooldown) - (1.0 / gun_cooldown)
-			stat_upgrade_effect(difference, attack_speed_val)
-			gun_cooldown = PlayerVariables.gun_cooldown
-
 func stat_upgrade_effect(difference, target):
-	print(difference)
+	var l = Label.new()
+	#l.text = "%.2f" % difference
+	l.text = "AAAAAAAAAAAAAAAAAAAA"
+	#var g_pos = Vector2(0, 0)
+
+	#print("size is ", size.x)
+	#g_pos.x = $StatDisplay.global_position.x + $StatDisplay.size.x
+	#g_pos.y = target.global_position.y
+
+	#print("g pos is ", g_pos)
+
+	#l.global_position = g_pos
+	#l.position = Vector2(0, 500)
+	#print("l.global = ", l.global_position)
+	l.position = target.position
+	l.position.x += $StatDisplay.size.x / 2
+	add_child(l)
+
+func _on_shop_item_purchased(item: ShopItem):
+	if item.name == "attack_speed_upgrade":
+		var difference = (1.0 / PlayerVariables.gun_cooldown) - (1.0 / gun_cooldown)
+		gun_cooldown = PlayerVariables.gun_cooldown
+		attack_speed_val.text = "%.2f shots / sec" % (1.0 / PlayerVariables.gun_cooldown)
+		# TODO: make sure rounding is the same so that
+		# the increase in the value matches the printed difference
+		stat_upgrade_effect(difference, attack_speed_val)
+		stat_upgrade_effect(difference, move_speed_val)
